a62a39d6de63ad1231fc8a70fe5a4c90a2d6da7e
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-06-11 02:20:43 +0200

    templating engine init

--- blog-node/index.js
+++ blog-node/index.js
@@ -2,23 +2,25 @@ const express = require("express");
 const path = require("path");
 
 const app = new express();
+const ejs = require("ejs");
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  res.render("contact");
 });
 
 app.get("/post", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  res.render("post");
 });
 
 app.listen(4000, () => {
